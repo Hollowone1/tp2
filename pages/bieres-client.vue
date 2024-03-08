0f0eefced6d67efe74aff5fe09ee751ba75b229b
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-08 17:06:26 +0100

    add images, still local path to find

--- pages/bieres-client.vue
+++ pages/bieres-client.vue
@@ -1,33 +1,30 @@
 <template>
     <div>
-      <h1> Liste des bières côtés serveur</h1>
-      <ul>
-        <li v-for="biere in bieres" :key="biere.id">{{ biere.name }}</li>
-      </ul>
+      <h2 v-for="biere in bieres" :key="biere.id">
+        <img :src="biere.image" />
+        <NuxtLink :to="`/bieres-client/${biere.id}`">{{ biere.name }}</NuxtLink>
+      </h2>
     </div>
   </template>
   
-  <script>
+  <script setup lang="ts">
   import { ref, onMounted } from 'vue';
+  import { useRoute } from 'vue-router';
   
-  export default {
-    setup() {
-      const bieres = ref([]);
+  const route = useRoute();
+  console.log(route.params.id);
   
-      const fetchBieres = async () => {
-        try {
-          const response = await fetch('https://api.sampleapis.com/beers/ale');
-          const data = await response.json();
-          bieres.value = data;
-        } catch (error) {
-          console.error(error);
-        }
-      };
+  const bieres = ref<any[]>([]);
   
-      onMounted(fetchBieres);
-      return {
-        bieres,
-      };
-    },
+  const fetchBieres = async () => {
+    try {
+      const response = await fetch('https://api.sampleapis.com/beers/ale');
+      const data = await response.json();
+      bieres.value = data;
+    } catch (error) {
+      console.error(error);
+    }
   };
+  
+  onMounted(fetchBieres);
   </script>
