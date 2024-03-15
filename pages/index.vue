121ee6c43f02330c61be687cd5c907c6e34c2ad5
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-15 15:28:57 +0100

    filtrer done

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,50 @@
 <template>
-    <div>
-        <h2>filtrer les bières</h2>
+  <div>
+    <h2>filtrer les bières</h2>
     <input type="number" v-model.number="priceMax" />
-    <button @click="filterBieres">Filtrer</button>
-    </div>
+    <button @click="filterBieres()">Filtrer</button>
     <div>
-      <h2 v-for="biere in bieres" :key="biere.id">
+      <h2 v-for="biere in filteredBieres" :key="biere.id">
         <img :src="biere.image" />
         <NuxtLink :to="`/bieres-client/${biere.id}`">{{ biere.name }}</NuxtLink>
         <p>{{biere.price}}</p>
       </h2>
-      
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref,computed, onMounted } from 'vue';
-  import { useRoute,useRouter } from 'vue-router';
-  
-  const route = useRoute();
-  const router = useRouter();
+  </div>
+</template>
 
-  const priceMax = ref<number>(route.query.pricemax ? parseFloat(route.query.pricemax as string) : 10);
-  const bieres = ref<any[]>([]); 
-  
-  const fetchBieres = async () => {
-    try {
-      const response = await fetch('https://api.sampleapis.com/beers/ale');
-      const data = await response.json();
-      bieres.value = data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  const bieresFiltered = computed(() => {
-  return bieres.value.filter((biere) => biere.price <= priceMax.value);
+<script setup lang="ts">
+import { ref, computed, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+const priceMax = ref<number | null>(route.query.pricemax ? parseFloat(route.query.pricemax as string) : null);
+const bieres = ref<any[]>([]);
+
+const fetchBieres = async () => {
+  try {
+    const response = await fetch('https://api.sampleapis.com/beers/ale');
+    const data = await response.json();
+    bieres.value = data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const filteredBieres = computed(() => {
+  if (priceMax.value) {
+    return bieres.value.filter(biere => Number(biere.price.replace('$', '')) <= priceMax.value);
+  } else {
+    return bieres.value;
+  }
 });
 
 const filterBieres = () => {
-  const query = { pricemax: priceMax.value.toString() };
+  const query = { pricemax: priceMax.value?.toString() || '' };
   router.push({ query });
 };
 
 onMounted(fetchBieres);
-  </script>
+</script>
