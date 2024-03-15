fe140831bf53a7e5f83270b83ade5703afc90b20
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-15 14:39:13 +0100

    routes

--- app.vue
+++ app.vue
@@ -2,7 +2,8 @@
   <div>
     <NuxtPage />
     <NuxtLink to="/type"></NuxtLink>
-    <NuxtLink to="/bieres-client">
-  </NuxtLink>
+    <NuxtLink to="/type/ale"></NuxtLink>
+    <NuxtLink to="/type/redale"></NuxtLink>
+    <NuxtLink to="/type/stouts"></NuxtLink>
   </div>
 </template>
