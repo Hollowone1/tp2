d8d5b850a7dd9fc0110a47cdfdec3b4bec9c03b1
Antonin <anto10800@gmail.com>
2024-03-15 15:05:36 +0100

    Bieres-serveurs && bieres-clients OK

--- pages/bieres-client/[id].vue
+++ pages/bieres-client/[id].vue
@@ -7,25 +7,22 @@ const route = useRoute()
 const id = route.params.id;
 const fetchBieres = async () => {
   try {
-    const response = await fetch(`https://api.sampleapis.com/beers/ale/${id}`);
-    biere.value = await response.json();
+      biere.value = await $fetch(`https://api.sampleapis.com/beers/ale/${id}`)
   } catch (error) {
     console.error(error);
   }
 };
 
 onMounted(fetchBieres);
+
 </script>
 
 <template>
-<!--  { "price": "$16.99", "name": "Founders All Day IPA", "rating": { "average": 4.411243509154233, "reviews": 453 }, "image": "https://www.totalwine.com/media/sys_master/twmmedia/h00/h94/11891416367134.png", "id": 1 }-->
   <div v-if="biere">
     <h1>Details de la bière</h1>
     <h2>{{ biere.name }}</h2>
     <img :src="biere.image" alt="Pas d'images" />
     <p>{{ biere.price }}</p>
-
-
   </div>
 </template>
 
